33e3123f5d9501dbd6ec69206cc64ad1c3d8625f
annejunkes <anneburkhardtjunkes@gmail.com>
2025-03-24 09:02:56 -0300

    imc

--- pdm-ifsc-master/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ pdm-ifsc-master/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -3,33 +3,40 @@ package com.ifsc.contaclick;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     Integer i=0;
+
+    EditText edPeso, edAltura;
+    TextView tvIMC;
+
+    Button b;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.textView);
-        tv.setText(getString(R.string.app_name));
+        edAltura =  findViewById(R.id.edAltura);
+        edPeso =  findViewById(R.id.edPeso);
+        tvIMC = findViewById(R.id.tvIMC);
+        b = findViewById(R.id.button);
 
-        Button b = findViewById(R.id.button);
+        b.setOnClickListener(v->{
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText(Integer.toString(i));
-                i++;
-            }
+            Double peso = Double.parseDouble(edPeso.getText().toString());
+            Double altura = Double.parseDouble(edAltura.getText().toString());
+
+            Double imc = peso/(altura*altura);
+
+            String imcTxt = imc.toString();
+            tvIMC.setText(imcTxt);
+
         });
 
 
